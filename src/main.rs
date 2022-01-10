3f9fd5601d3c377493fb01afbbd02168097d06d1
taddes <taddes.korris@gmail.com>
2022-01-10 18:24:46 -0500

    audio function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rusty_audio::Audio;
+
+fn main() -> Result <(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("move", "move.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("win", "win.wav");
+    audio.add("startup", "startup.wav");
+    audio.play("startup");
+
+    // Cleanup - run off main - audio sys plays on seperate thread in parallel
+    audio.wait();
+    Ok(())
 }
